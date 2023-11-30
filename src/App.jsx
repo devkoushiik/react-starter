e4186682cbc9a19911ffb7463d745e8680a65731
Koushik Ahmed <koushikxahmed@gmail.com>
2023-11-30 14:51:25 +0600

    initial setup done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,4 @@
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Skills />
-      <About />
-      <Projects />
-      <Footer />
-    </>
-  );
+  return <div>App</div>;
 };
 export default App;
