2fa0591f38325c38e99f45fa66da0cbf8d392e91
koushikxahmed <koushikxahmed@gmail.com>
2024-08-07 18:47:26 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="relative h-full overflow-y-auto antialiased">
       <div className="fixed inset-0 bg-fixed bg-cover bg-center bg-img">
         <div className="relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto">
+          <Navbar />
           <Hero />
         </div>
       </div>
