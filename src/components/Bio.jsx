06ac77f4107a0dee0f2e7f68e37d523c378d7351
koushikxahmed <koushikxahmed@gmail.com>
2024-08-08 13:22:28 +0600

    work experience section done

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -6,7 +6,7 @@ const Bio = () => {
       <h2 className="text-center text-3xl lg:text-4xl">Bio</h2>
       <div>
         {BIO.map((bio, index) => (
-          <p key={index} className="mb-4 text-lg lg:text-4xl">
+          <p key={index} className="mb-4 text-lg lg:text-xl">
             {bio}
           </p>
         ))}
