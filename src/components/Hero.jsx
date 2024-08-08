fce4c4edd0cdb2b415a8babe0fc04c626319ddae
koushikxahmed <koushikxahmed@gmail.com>
2024-08-08 20:42:59 +0600

    hero animation done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
 import { HERO } from "../constants";
 import carlImg from "../assets/carl.jpeg";
+import { motion } from "framer-motion";
+
 const Hero = () => {
   return (
     <div className="flex min-h-screen flex-wrap items-center">
-      <div className="w-full md:w-1/2">
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="w-full md:w-1/2"
+      >
         <h2 className="my-8 p-2 text-4xl font-bold md:text-5xl lg:text-[7rem]">
           {HERO.name}
           {/* <span className="text-md ml-6">{HERO.lastName}</span> */}
@@ -12,17 +19,25 @@ const Hero = () => {
           {HERO.greet}
         </p>
         <p className="mb-8 p-2 text-xl">{HERO.description}</p>
-      </div>
+      </motion.div>
       <div className="w-full md:w-1/2 lg:p-8">
-        <div className="flex justify-end">
-          <img
+        <motion.div
+          initial={{ opacity: 0, scale: 0.6 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
+          className="flex justify-end"
+        >
+          <motion.img
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
             src={carlImg}
             width={550}
             height={550}
             alt="Carl Frank"
             className="rounded-3xl"
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
